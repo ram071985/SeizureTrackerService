1a3d4a0f790d6ff1a6c986f4b627d886527cfe7a
ram071985 <reidmuchow@gmail.com>
2026-02-07 19:35:17 -0600

    safety commit

--- SeizureTrackerService/obj/Debug/net10.0/SeizureTrackerService.AssemblyInfo.cs
+++ SeizureTrackerService/obj/Debug/net10.0/SeizureTrackerService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeizureTrackerService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49eb2bcc0e3134464bf954911eff558da4673e6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fb4e9a6ad02b8198be8e4f7ea96154351ba4a17")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeizureTrackerService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeizureTrackerService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
