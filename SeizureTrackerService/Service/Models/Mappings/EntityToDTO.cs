c296b028ada300227f91337c7815f5c8a4581a44
ram071985 <reidmuchow@gmail.com>
2026-02-24 22:14:03 -0600

    adds back time zone info convert time.

--- SeizureTrackerService/Service/Models/Mappings/EntityToDTO.cs
+++ SeizureTrackerService/Service/Models/Mappings/EntityToDTO.cs
@@ -16,7 +16,7 @@ internal static class EntityToDTO
         return new SeizureActivityHeaderDTO()
         {
             Id = entity.Id,
-            Date = entity.Date.DateTime.ToString("D"),
+            Date = TimeZoneInfo.ConvertTime(entity.Date.DateTime, cstZone).ToString("D"),
             DailyTotal = entity.DailyTotal
         };
     }
@@ -33,7 +33,7 @@ internal static class EntityToDTO
         {
             SeizureId = entity.SeizureId,
             LogId = entity.LogId,
-            SeizureTime = entity.SeizureTime.DateTime.ToString("h:mm tt"),
+            SeizureTime = TimeZoneInfo.ConvertTime(entity.SeizureTime.DateTime, cstZone).ToString("h:mm tt"),
             SeizureType = entity.SeizureType,
             Comments = entity.Comments
         };
